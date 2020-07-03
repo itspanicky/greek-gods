e9287e183d204bcbd807e95d55db593750d5331d
itspanicky <nicky.li92@gmail.com>
2020-07-02 23:57:11 -0400

    fix: missing comma

--- models/Abode.js
+++ models/Abode.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const AbodeSchema = new Schema({
   name: { type: String, required: true, unique: true },
   coordinates: { type: String },
-  gods { type: Array },
+  gods: { type: Array }
 });
 
 module.exports = mongoose.model("abode", AbodeSchema);
