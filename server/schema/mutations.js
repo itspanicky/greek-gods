6f108229157316101f243de86540dbe24c15d085
itspanicky <nicky.li92@gmail.com>
2020-07-20 21:10:25 -0400

    feat: mutations to create and delete gods

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -5,8 +5,29 @@ const God = mongoose.model("god");
 const GodType = require("./god_type");
 
 const mutation = new GraphQLObjectType({
-    name: "Mutation",
-    fields: {}
+  name: "Mutation",
+    fields: {
+      newGod: {
+        type: GodType,
+        args: {
+          name: { type: GraphQLString },
+          type: { type: GraphQLString },
+          description: { type: GraphQLString },
+        },
+        resolve(parentValue, { name, type, description }) {
+          return new God({ name, type, description }).save();
+        }
+      },
+      deleteGod: {
+          type: GodType,
+          args: {
+              id: { type: GraphQLString }
+          },
+          resolve(parentValue, { id }) {
+              return God.deleteOne({ id })
+          }
+      }
+    }
 });
 
 module.exports = mutation;
