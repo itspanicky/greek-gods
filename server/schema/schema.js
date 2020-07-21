dcf3ef99f652b137e52a16890366802f646f1af9
itspanicky <nicky.li92@gmail.com>
2020-07-20 21:02:20 -0400

    feat: import mutations to schema file

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,10 +2,12 @@ const graphql = require("graphql");
 const { GraphQLSchema } = graphql;
 
 const RootQueryType = require("./root_query_type");
+const mutations = require("./mutations");
 
 // create GraphQL schema instance by handing `GraphQLSchema` a root query
 
 // export to server.js for configuring the ExpressGraphQL middleware
 module.exports = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: mutations
 })
