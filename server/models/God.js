c9bdf1e8b4a95d2e4a7cbe0c42dc807e6e74a8d2
itspanicky <nicky.li92@gmail.com>
2020-07-24 00:09:54 -0400

    feat: mutation to remove god relative

--- server/models/God.js
+++ server/models/God.js
@@ -14,10 +14,66 @@ const GodSchema = new Schema({
   siblings: [ { type: Schema.Types.ObjectId, ref: "god", }, ],
 });
 
+// Static methods can be called anywhere in our app 
+// where we have access to the God model
 GodSchema.statics.findRelatives = function(godId, type) {
   return this.findById(godId)
     .populate(`${type}`)
     .then(god => god[type])
+}
+
+GodSchema.statics.addRelative = function(godId, relativeId, relationship) {
+  return this.find({ 
+    _id: { $in: [godId, relativeId] } 
+  }).then(gods => {
+    // $in allows us to query for multiple properties at once
+    // and returns results as an array
+    const god = godId === gods[0].id ? gods[0] : gods[1];
+    const relative = relativeId === gods[0].id ? gods[0] : gods[1];
+
+    switch (relationship) {
+      case "parent":
+        god.parents.push(relative);
+        relative.children.push(god);
+        break;
+      case "child":
+        god.children.push(relative);
+        relative.parents.push(god);
+        break;
+      case "sibling":
+        god.siblings.push(relative);
+        relative.siblings.push(god);
+        break;
+    }
+
+    return Promise.all([god.save(), relative.save()]).then(
+      ([god, relative]) => god
+    );
+  })
+}
+
+GodSchema.statics.removeRelative = function(godId, relativeId, relationship) {
+  return this.find({
+    _id: { $in: [godId, relativeId] }
+  }).then(gods => {
+    const god = godId === gods[0].id ? gods[0] : gods[1];
+    const relative = relativeId === gods[0].id ? gods[0] : gods[1];
+
+    switch (relationship) {
+      case "parent":
+        god.parents.pull(relative);
+        relative.children.pull(god);
+        break;
+      case "child":
+        god.children.pull(relative);
+        relative.parents.pull(god);
+        break;
+      case "sibling":
+        god.siblings.pull(relative);
+        relative.siblings.pull(god);
+        break;
+    }
+  })
 }
 
 module.exports = mongoose.model("god", GodSchema);
--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -6,47 +6,73 @@ const GodType = require("./god_type");
 
 const mutation = new GraphQLObjectType({
   name: "Mutation",
-    fields: {
-      newGod: {
+  fields: {
+    newGod: {
+      type: GodType,
+      args: {
+        name: { type: GraphQLString },
+        type: { type: GraphQLString },
+        description: { type: GraphQLString },
+      },
+      resolve(parentValue, { name, type, description }) {
+        return new God({ name, type, description }).save();
+      },
+    },
+    deleteGod: {
+      type: GodType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parentValue, { id }) {
+        return God.findByIdAndDelete({ _id: id });
+      },
+    },
+    updateGod: {
+      type: GodType,
+      args: {
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        type: { type: GraphQLString },
+        description: { type: GraphQLString },
+      },
+      resolve(parentValue, { id, name, type, description }) {
+        const updateObj = {};
+        if (name) updateObj.name = name;
+        if (type) updateObj.type = type;
+        if (description) updateObj.description = description;
+
+        return God.findOneAndUpdate(
+          { _id: id },
+          { $set: updateObj },
+          { $new: true },
+          (err, god) => {
+            return god;
+          }
+        );
+      }
+    },
+    addGodRelative: {
+      type: GodType,
+      args: {
+        godId: { type: GraphQLID },
+        relativeId: { type: GraphQLID },
+        relationship: { type: GraphQLString },
+      },
+      resolve(parentValue, { godId, relativeId, relationship }) {
+        return God.addRelative(godId, relativeId, relationship);
+      }
+    },
+    removeGodRelative: {
         type: GodType,
         args: {
-          name: { type: GraphQLString },
-          type: { type: GraphQLString },
-          description: { type: GraphQLString },
+            godId: { type: GraphQLID },
+            relativeId: { type: GraphQLID },
         },
-        resolve(parentValue, { name, type, description }) {
-          return new God({ name, type, description }).save();
+        resolve(parentValue, { godId, relativeId }) {
+            return God.removeRelative(godId, relativeId);
         }
-      },
-      deleteGod: {
-          type: GodType,
-          args: {
-              id: { type: GraphQLID }
-          },
-          resolve(parentValue, { id }) {
-              return God.findByIdAndDelete({ _id: id });
-          }
-      },
-      updateGod: {
-          type: GodType,
-          args: {
-              id: { type: GraphQLID },
-              name: { type: GraphQLString },
-              type: { type: GraphQLString },
-              description: { type: GraphQLString }
-          },
-          resolve(parentValue, { id, name, type, description }) {
-            const updateObj = {};
-            if (name) updateObj.name = name;
-            if (type) updateObj.type = type;
-            if (description) updateObj.description = description;
-
-            return God.findOneAndUpdate({ _id: id }, { $set: updateObj }, { $new: true }, (err, god) => {
-                return god
-            });
-          }
-      }
     }
+  },
 });
 
 module.exports = mutation;
