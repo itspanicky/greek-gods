006694895c577dcbd1b463fb7f17173d239d62d9
itspanicky <nicky.li92@gmail.com>
2020-08-17 19:43:16 -0400

    setup apollo

--- client/index.js
+++ client/index.js
@@ -1,8 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+// since we know our ids from Mongo will be unique we can use the object.id here
+// for mapping our state shape within the InMemoryCache
+// now the cache state shape will be {object.id: object}
+const cache = new InMemoryCache({
+  dataIdFromObject: (object) => object.id || null,
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  // using the cache we just created
+  cache: cache,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log("graphQLErrors", graphQLErrors);
+    console.log("networkError", networkError);
+  },
+});
 
 const Root = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <ApolloProvider client={client}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
