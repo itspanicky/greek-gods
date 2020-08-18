b85d3b08d10f00c5c53398a82c07385a427420d4
itspanicky <nicky.li92@gmail.com>
2020-08-18 19:19:57 -0400

    feat: delete god mutation

---
+++ client/graphql/mutations.js
@@ -0,0 +1,13 @@
+import gql from "graphql-tag";
+
+export default {
+  // we make sure to use the `mutation` key word when creating our frontend mutations
+  // just like we would in GraphiQL
+  DELETE_GOD: gql`
+    mutation DeleteGod($id: ID) {
+      deleteGod(id: $id) {
+        id
+      }
+    }
+  `,
+};
