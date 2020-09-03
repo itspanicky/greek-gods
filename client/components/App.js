4fc0679567bd09ca3eb15ae5583baf5e66d5c252
itspanicky <nicky.li92@gmail.com>
2020-09-03 18:32:19 -0400

    feat: create-emblem route

--- client/components/App.js
+++ client/components/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import GodsList from "./gods/GodsList";
 import GodCreate from "./create/GodCreate";
+import EmblemCreate from "./create/EmblemCreate";
+
 
 const App = () => {
   return (
     <div>
+      <Route exact path="/create-emblem" component={EmblemCreate} />
       <Route exact path="/create-god" component={GodCreate} />
       <Route exact path="/" component={GodsList} />
     </div>
