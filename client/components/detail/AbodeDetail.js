99a65867aeb9b6c7620147ce22b7cbaadad7245a
itspanicky <nicky.li92@gmail.com>
2020-09-16 22:01:13 -0400

    feat: setup editing abode detail

--- client/components/detail/AbodeDetail.js
+++ client/components/detail/AbodeDetail.js
@@ -5,14 +5,36 @@ import { FaPencilAlt } from "react-icons/fa";
 import Mutations from "../../graphql/mutations";
 import Queries from "../../graphql/queries";
 
+const { FETCH_ABODES } = Queries;
+
+
 const AbodeDetail = props => {
+  const [editing, setEditing] = useState(false);
   const [abode, setAbode] = useState(props.abode || "");
   console.log("abode", abode);
-  return (
-    <div>
-      <p>Abode: {abode.name}</p>
-    </div>
-  )
+
+  if (editing) {
+    return (
+      <Mutation>
+
+      </Mutation>
+
+    )
+  } else {
+    return (
+      <div>
+        <div
+          onClick={() => setEditing(true)}
+          style={{ fontSize: "10px", cursor: "pointer", display: "inline" }}
+        >
+          <IconContext.Provider value={{ className: "custom-icon" }}>
+            <FaPencilAlt />
+          </IconContext.Provider>
+        </div>
+        <p style={{ display: "inline" }}>Abode: {abode.name}</p>
+      </div>
+    );
+  }
 }
 
 export default AbodeDetail;
